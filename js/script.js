ebcadd47c78d344bababd9b4931c0f015e834261
SnowMochi <122312973+snowymochi@users.noreply.github.com>
2023-03-03 20:16:11 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
